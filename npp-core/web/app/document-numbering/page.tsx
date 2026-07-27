d41f979c87214dcdf0366c85e5d006f18a9960ef
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-07-27 18:40:20 +0700

    Phase 3.3F: add document numbering page

---
+++ npp-core/web/app/document-numbering/page.tsx
@@ -0,0 +1,7 @@
+import DocumentNumberingWorkspace from './document-numbering-workspace';
+
+export const dynamic = 'force-dynamic';
+
+export default function DocumentNumberingPage() {
+  return <DocumentNumberingWorkspace />;
+}
