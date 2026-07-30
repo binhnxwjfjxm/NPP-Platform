d7a75a8d8342203e54031fc7c913411d13f03d0a
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-07-30 09:30:55 +0700

    fix(migrations): register supplier payment series lifecycle

--- npp-core/api/src/migrations/index.js
+++ npp-core/api/src/migrations/index.js
@@ -1,19 +1,19 @@
 import { readFileSync } from 'node:fs';
 import {
-  CORE_API_MIGRATIONS as CORE_API_MIGRATIONS_THROUGH_031,
+  CORE_API_MIGRATIONS as CORE_API_MIGRATIONS_THROUGH_032,
   runMigrations,
-} from './index-through-031.js';
+} from './index-through-032.js';
 
-const SUPPLIER_PAYMENT_ALLOCATION_HARDENING_SQL = readFileSync(
-  new URL('../../../../database/migrations/accounting/032_supplier_payment_allocation_hardening.sql', import.meta.url),
+const SUPPLIER_PAYMENT_SERIES_LIFECYCLE_SQL = readFileSync(
+  new URL('../../../../database/migrations/accounting/033_supplier_payment_series_lifecycle.sql', import.meta.url),
   'utf8',
 );
 
 export const CORE_API_MIGRATIONS = Object.freeze([
-  ...CORE_API_MIGRATIONS_THROUGH_031,
+  ...CORE_API_MIGRATIONS_THROUGH_032,
   Object.freeze({
-    id: '032_supplier_payment_allocation_hardening',
-    sql: SUPPLIER_PAYMENT_ALLOCATION_HARDENING_SQL,
+    id: '033_supplier_payment_series_lifecycle',
+    sql: SUPPLIER_PAYMENT_SERIES_LIFECYCLE_SQL,
   }),
 ]);
 
