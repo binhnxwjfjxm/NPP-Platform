2d09fbb73ebead5ba2b0a49231e23a34f7488c99
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-07-30 09:45:06 +0700

    fix(migrations): register numbering namespace capacity

--- npp-core/api/src/migrations/index.js
+++ npp-core/api/src/migrations/index.js
@@ -1,19 +1,19 @@
 import { readFileSync } from 'node:fs';
 import {
-  CORE_API_MIGRATIONS as CORE_API_MIGRATIONS_THROUGH_032,
+  CORE_API_MIGRATIONS as CORE_API_MIGRATIONS_THROUGH_033,
   runMigrations,
-} from './index-through-032.js';
+} from './index-through-033.js';
 
-const SUPPLIER_PAYMENT_SERIES_LIFECYCLE_SQL = readFileSync(
-  new URL('../../../../database/migrations/accounting/033_supplier_payment_series_lifecycle.sql', import.meta.url),
+const DOCUMENT_NUMBER_IDEMPOTENCY_NAMESPACE_SQL = readFileSync(
+  new URL('../../../../database/migrations/accounting/034_document_number_idempotency_namespace.sql', import.meta.url),
   'utf8',
 );
 
 export const CORE_API_MIGRATIONS = Object.freeze([
-  ...CORE_API_MIGRATIONS_THROUGH_032,
+  ...CORE_API_MIGRATIONS_THROUGH_033,
   Object.freeze({
-    id: '033_supplier_payment_series_lifecycle',
-    sql: SUPPLIER_PAYMENT_SERIES_LIFECYCLE_SQL,
+    id: '034_document_number_idempotency_namespace',
+    sql: DOCUMENT_NUMBER_IDEMPOTENCY_NAMESPACE_SQL,
   }),
 ]);
 
