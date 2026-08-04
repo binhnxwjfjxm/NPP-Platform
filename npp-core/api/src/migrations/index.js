fd80308ed787e70fddb09d19a0e4478934a9b8bd
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-08-04 17:09:20 +0700

    fix(logistics): gom constraint vào migration 046

--- npp-core/api/src/migrations/index.js
+++ npp-core/api/src/migrations/index.js
@@ -4,10 +4,16 @@ import {
   runMigrations,
 } from './index-through-045.js';
 
-const LOGISTICS_TRIP_PLANNING_SQL = readFileSync(
-  new URL('../../../../database/migrations/logistics/046_logistics_trip_planning.sql', import.meta.url),
-  'utf8',
-);
+const LOGISTICS_TRIP_PLANNING_SQL = [
+  readFileSync(
+    new URL('../../../../database/migrations/logistics/046_logistics_trip_planning.sql', import.meta.url),
+    'utf8',
+  ),
+  readFileSync(
+    new URL('../../../../database/migrations/logistics/046_logistics_trip_planning_constraints.sql', import.meta.url),
+    'utf8',
+  ),
+].join('\n\n');
 
 export const CORE_API_MIGRATIONS = Object.freeze([
   ...CORE_API_MIGRATIONS_THROUGH_045,
