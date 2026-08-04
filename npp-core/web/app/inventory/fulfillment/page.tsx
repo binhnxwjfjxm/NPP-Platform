e0777b2ac23dca9ce80b00b0cc5fac5a2b9257ae
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-08-04 10:55:48 +0700

    feat(npp): thêm màn chuẩn bị hàng

---
+++ npp-core/web/app/inventory/fulfillment/page.tsx
@@ -0,0 +1,7 @@
+import FulfillmentWorkspace from './fulfillment-workspace';
+
+export const dynamic = 'force-dynamic';
+
+export default function FulfillmentPage() {
+  return <FulfillmentWorkspace />;
+}
