7e1a193044abaa9500e1d252259317fce5530269
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-08-06 01:45:49 +0700

    feat(api): route return credit and refund requests

--- npp-core/api/src/routes/customer-receivables.js
+++ npp-core/api/src/routes/customer-receivables.js
@@ -1,7 +1,9 @@
+import { handleCustomerReturnCreditRoutes } from './customer-return-credits.js';
 import { handleCustomerPaymentRoutes } from './customer-payments.js';
 import { handleCustomerReceivableCoreRoutes } from './customer-receivables-core.js';
 
 export async function handleCustomerReceivableRoutes(req, res, options) {
+  if (await handleCustomerReturnCreditRoutes(req, res, options)) return true;
   if (await handleCustomerPaymentRoutes(req, res, options)) return true;
   return handleCustomerReceivableCoreRoutes(req, res, options);
 }
