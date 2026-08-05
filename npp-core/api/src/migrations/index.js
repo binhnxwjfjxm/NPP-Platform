442b615bd63e3d302ee68f9a219993b9a6e99f6b
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-08-06 00:36:25 +0700

    fix(api): include customer payment hardening SQL

--- npp-core/api/src/migrations/index.js
+++ npp-core/api/src/migrations/index.js
@@ -29,10 +29,10 @@ const CUSTOMER_RECEIVABLE_LEDGER_SQL = [
   readFileSync(new URL('../../../../database/migrations/accounting/053_customer_receivable_ledger.sql', import.meta.url), 'utf8'),
   readFileSync(new URL('../../../../database/migrations/accounting/053_customer_receivable_pickup_reversal.sql', import.meta.url), 'utf8'),
 ].join('\n\n');
-const CUSTOMER_PAYMENT_ALLOCATION_SQL = readFileSync(
-  new URL('../../../../database/migrations/accounting/054_customer_payment_allocation.sql', import.meta.url),
-  'utf8',
-);
+const CUSTOMER_PAYMENT_ALLOCATION_SQL = [
+  readFileSync(new URL('../../../../database/migrations/accounting/054_customer_payment_allocation.sql', import.meta.url), 'utf8'),
+  readFileSync(new URL('../../../../database/migrations/accounting/054_customer_payment_allocation_hardening.sql', import.meta.url), 'utf8'),
+].join('\n\n');
 
 export const CORE_API_MIGRATIONS = Object.freeze([
   ...CORE_API_MIGRATIONS_THROUGH_045,
