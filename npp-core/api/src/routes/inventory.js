dfd258e7f15d853e89b250adf2e156ad963f9e5d
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-08-05 09:36:09 +0700

    feat(logistics): route POD APIs

--- npp-core/api/src/routes/inventory.js
+++ npp-core/api/src/routes/inventory.js
@@ -3,12 +3,17 @@ import { handleLogisticsRoutes } from './logistics.js';
 import { handleLogisticsAttemptRoutes } from './logistics-attempts.js';
 import { handleLogisticsDispatchRoutes } from './logistics-dispatch.js';
 import { handleLogisticsDriverRoutes } from './logistics-driver.js';
+import { handleLogisticsPodRoutes } from './logistics-pod.js';
 import { handleLogisticsReconciliationRoutes } from './logistics-reconciliation.js';
 
 // Compatibility markers: handleFulfillmentOperationRoutes and handleDeliveryOrderRoutes
 // remain owned by inventory-core.js; this wrapper adds Logistics namespaces.
 export async function handleInventoryRoutes(req, res, options) {
   const pathname = new URL(`http://localhost${req.url}`).pathname;
+  if (/^\/api\/logistics\/driver\/trips\/[^/]+\/assignments\/[^/]+\/attempts\/[^/]+\/pod$/.test(pathname)
+      || /^\/api\/logistics\/trips\/[^/]+\/attempts\/[^/]+\/pod$/.test(pathname)) {
+    return handleLogisticsPodRoutes(req, res, options);
+  }
   if (pathname === '/api/logistics/driver/trips'
       || pathname.startsWith('/api/logistics/driver/trips/')) {
     return handleLogisticsDriverRoutes(req, res, options);
