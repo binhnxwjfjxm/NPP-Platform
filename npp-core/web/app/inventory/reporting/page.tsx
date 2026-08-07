c73f74d738d52b65e9d8a39fca14538c28bc595e
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-08-07 18:28:14 +0700

    test(reporting): lock Phase 8.2 route and source contracts

---
+++ npp-core/web/app/inventory/reporting/page.tsx
@@ -0,0 +1,5 @@
+import { InventoryReportingWorkspace } from '../../components/inventory-reporting-workspace';
+
+export default function InventoryReportingPage() {
+  return <InventoryReportingWorkspace />;
+}
