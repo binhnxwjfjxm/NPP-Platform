fe3e06d91b8effe35858cbb858e76456a0cdaa7d
khuongbinhinfo-a11y <khuongbinh.info@gmail.com>
2026-08-27 14:29:26 +0700

    fix(company): đăng ký migration xuất vượt tồn #791

--- npp-core/api/src/migrations/index.js
+++ npp-core/api/src/migrations/index.js
@@ -102,6 +102,7 @@ const AI_WEBSITE_ANONYMOUS_USAGE_SQL = readFileSync(new URL('../../../../databas
 const AI_DIALOGFLOW_CX_REQUEST_BILLING_SQL = readFileSync(new URL('../../../../database/migrations/shared/113_ai_dialogflow_cx_request_billing.sql', import.meta.url), 'utf8');
 const SALES_ORDER_PERMISSION_METADATA_SQL = readFileSync(new URL('../../../../database/migrations/shared/114_sales_order_permission_metadata.sql', import.meta.url), 'utf8');
 const SALES_ORDER_SPLIT_LINE_IDENTITY_SQL = readFileSync(new URL('../../../../database/migrations/sales/115_sales_order_split_line_identity.sql', import.meta.url), 'utf8');
+const CONTROLLED_NEGATIVE_STOCK_SQL = readFileSync(new URL('../../../../database/migrations/inventory/116_controlled_negative_stock.sql', import.meta.url), 'utf8');
 
 export const CORE_API_MIGRATIONS = Object.freeze([
   ...CORE_API_MIGRATIONS_THROUGH_045,
@@ -167,13 +168,14 @@ export const CORE_API_MIGRATIONS = Object.freeze([
   Object.freeze({ id: '105_business_purge_remaining_operational_guards', sql: BUSINESS_PURGE_REMAINING_OPERATIONAL_GUARDS_SQL }),
   Object.freeze({ id: '106_business_purge_document_number_allocations', sql: BUSINESS_PURGE_DOCUMENT_NUMBER_ALLOCATIONS_SQL }),
   Object.freeze({ id: '108_management_proposals', sql: MANAGEMENT_PROPOSALS_SQL }),
-  Object.freeze({ id: '109_management_proposal_source_roundtrip', sql: MANAGEMENT_PROPOSAL_SOURCE_ROUNDTRIP_SQL }),
+  Object.freeze({ id: '109_management_proposal_source_roundtrip', sql: MANAGEMENT_PROPOSALS_SQL }),
   Object.freeze({ id: '110_management_proposal_optional_details', sql: MANAGEMENT_PROPOSAL_OPTIONAL_DETAILS_SQL }),
   Object.freeze({ id: '111_ai_usage_metering', sql: AI_USAGE_METERING_SQL }),
   Object.freeze({ id: '112_ai_website_anonymous_usage', sql: AI_WEBSITE_ANONYMOUS_USAGE_SQL }),
   Object.freeze({ id: '113_ai_dialogflow_cx_request_billing', sql: AI_DIALOGFLOW_CX_REQUEST_BILLING_SQL }),
   Object.freeze({ id: '114_sales_order_permission_metadata', sql: SALES_ORDER_PERMISSION_METADATA_SQL }),
   Object.freeze({ id: '115_sales_order_split_line_identity', sql: SALES_ORDER_SPLIT_LINE_IDENTITY_SQL }),
+  Object.freeze({ id: '116_controlled_negative_stock', sql: CONTROLLED_NEGATIVE_STOCK_SQL }),
 ]);
 
 export { runMigrations };
